646a9048362408b7b9faeb019160a4a5f38236b9
gdm-1718-annadela <annadela@student.arteveldehs.be>
2018-10-10 12:01:19 +0200

    lesweek3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gdmgent_1819_csse1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lesweken/lesweek3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lesweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Het afdrukken van een tekst in de console.
+            Console.WriteLine("Dag!");
+
+            /* Dus deze applicatie doet momenteel nog niet veel */
+
+            //REKENMACHINE
+                //int c, d;
+                int a = 5;
+                int b = 6;
+
+                int resultaat1 = optellen(a, b);
+                int resultaat2 = deling(a, b);
+                int restulaat3 = verhogen(a);
+        }
+
+        static int optellen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int deling(int a, int b)
+        {
+            return a / b;
+        }
+
+        static int verhogen(int a)
+        {
+            return a++;
+        }
+    }
+}
