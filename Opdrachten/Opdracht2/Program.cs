8067ec7802e7079233b885a60eb248bb19731820
gdm-1718-annadela <annadela@student.arteveldehs.be>
2018-10-17 12:56:26 +0200

    Opdracht 2

--- Opdrachten/Opdracht2/Program.cs
+++ Opdrachten/Opdracht2/Program.cs
@@ -7,8 +7,25 @@ namespace Opdracht2
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
-            for(int i=0; i<10; i++){
-                Console.WriteLine(i);
+            //Faculteit
+            int getal=10;
+            int fac = getal;
+            while(getal >2){
+                getal -= 1;
+                fac *= getal;
+            };
+            Console.WriteLine("De faculteit van het ingevoerde getal is " + fac);
+
+            //Rij van Fibonacci
+            int firstNum=0;
+            int sum=1;
+            for(int fib = 0; fib<10; fib++){
+                sum = firstNum + sum;
+                Console.WriteLine(firstNum);
+                Console.WriteLine(sum);
+                firstNum= firstNum + sum;
+                // secondNum++;
+                
             }
 
         }
