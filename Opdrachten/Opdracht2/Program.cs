b4e39219e46bff84598de62deb1fbfd768494c8f
gdm-1718-annadela <annadela@student.arteveldehs.be>
2018-11-14 10:03:36 +0100

    test3

---
+++ Opdrachten/Opdracht2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alle tafels.
+            for(int getaltafel =0; getaltafel<11; getaltafel++){
+                Console.WriteLine("tafel van " + getaltafel);
+                for(int getaltafeltwee = 0; getaltafeltwee< 11; getaltafeltwee ++){
+                    Console.WriteLine(getaltafel + " x "+  getaltafeltwee + " = " + getaltafel * getaltafeltwee);
+                }
+            }
+            //Tafel van gekozen getal.
+            Program program = new Program();
+            Console.WriteLine("kies een getal voor uw tafel te kiezen");
+            int tafelVan = Convert.ToInt32(Console.ReadLine());
+            program.Tafel(tafelVan);
+
+            //Faculteit van gekozen getal.
+            Console.WriteLine("kies getal voor uw faculteit te verkrijgen");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            program.Faculteit(getal);
+
+            //Rij van Fibonacci tot gekozen getal.
+            Console.WriteLine("kies uw limiet");
+            int getalFib = Convert.ToInt32(Console.ReadLine());
+            program.Fibonacci(getalFib);
+        }
+            void Tafel(int tafelgetal){
+            Console.WriteLine("De tafel van " + tafelgetal);
+            for(int firstNum = 0; firstNum<11; firstNum++){
+                int resultaat = firstNum * tafelgetal;
+                Console.WriteLine(firstNum + " x " + tafelgetal + " = " + resultaat);
+                }
+            }
+
+            void Faculteit(int getal){
+                int fac = getal;
+                while(getal >2){
+                    getal -= 1;
+                    fac *= getal;
+                };
+            Console.WriteLine("De faculteit van het ingevoerde getal is " + fac);
+            }
+
+            void Fibonacci(int getalFib){
+                int firstNumFib=0;
+                int sum=1;
+                for(int fib = 0; fib<getalFib; fib++){
+                    sum = firstNumFib + sum;
+                    Console.WriteLine(firstNumFib);
+                    Console.WriteLine(sum);
+                    firstNumFib= firstNumFib + sum;
+                }
+            }
+        }
+    }
+
---
+++ Opdrachten/Opdracht3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Opdracht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opdracht 1.
+                //Menu vertonen.
+                Dictionary<string, double> menu = new Dictionary<string, double>();
+                menu.Add("plat water", 1);
+                menu.Add("cola 25cl", 1.5);
+                menu.Add("cola 33cl", 2);
+                menu.Add("witte wijn", 3);
+                menu.Add("pils", 2);
+                menu.Add("toast", 5);
+                menu.Add("kaasplank", 4);
+
+                Console.WriteLine("het menu");
+                foreach(var menulist  in menu){
+                    Console.WriteLine(menulist.Key + "\t" + menulist.Value);
+                }
+
+                //Gesorteerd menu vetertonen.
+                SortedDictionary<string, double> sortedMenu = new SortedDictionary<string, double>();
+                sortedMenu.Add("plat water", 1);
+                sortedMenu.Add("cola 25cl", 1.5);
+                sortedMenu.Add("cola 33cl", 2);
+                sortedMenu.Add("witte wijn", 3);
+                sortedMenu.Add("pils", 2);
+                sortedMenu.Add("toast", 5);
+                sortedMenu.Add("kaasplank", 4);
+
+                Console.WriteLine();
+                Console.WriteLine("het gesorteerd menu");
+                foreach(var menulist  in sortedMenu){
+                    Console.WriteLine(menulist.Key + "\t" + menulist.Value);
+                }
+
+                //Kiezen van artiekel.
+                Console.WriteLine("kies uw bestelling");
+                string bestelling = Console.ReadLine();
+                Console.WriteLine("u koos voor " + bestelling);
+                Console.WriteLine("dit kost " + sortedMenu[bestelling]+" euro");
+                Console.WriteLine();
+
+            //Opdracht 2.
+                //Gekozen datatype Queue.
+                Console.WriteLine("lijst van nog niet bediende klanten");
+                Queue list = new Queue();
+                    list.Enqueue("Anna");
+                    list.Enqueue("Bert");
+                    list.Enqueue("Ciel");
+                    list.Enqueue("Dries");
+                    list.Enqueue("Eva");
+                    list.Enqueue("Frederic");
+                    list.Enqueue("Gert");
+
+                //Een per 1 verwijderen.
+                for(int i=0; i<6; i++){
+                    Console.WriteLine("lijst van nog niet bediende klanten");
+                    foreach(Object obj in list){
+                        
+                        Console.WriteLine(obj);  
+                    }
+                    list.Dequeue();
+                    Console.WriteLine();
+            }
+        }
+    }
+}
